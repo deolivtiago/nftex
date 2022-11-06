3a82342668162a5ac431713c5ad75894f0c36252
Tiago de Oliveira <deoliv.tiago@proton.me>
2022-11-06 01:16:36 -0300

    feat: update facade to list arts

--- lib/nftex/arts.ex
+++ lib/nftex/arts.ex
@@ -1,7 +1,9 @@
 defmodule Nftex.Arts do
   alias Nftex.Arts.Create
   alias Nftex.Arts.Get
+  alias Nftex.Arts.All
 
   defdelegate create(attrs), to: Create, as: :call
   defdelegate get(id), to: Get, as: :call
+  defdelegate all, to: All, as: :call
 end
