6f057a9f87dc61fcdadfb8f28aa28120a7161ac7
Tiago de Oliveira <deoliv.tiago@proton.me>
2022-11-06 00:30:52 -0300

    feat: add input object notation

--- lib/nftex_web/schema/types/art.ex
+++ lib/nftex_web/schema/types/art.ex
@@ -2,10 +2,17 @@ defmodule NftexWeb.Schema.Types.Art do
   use Absinthe.Schema.Notation
 
   object :art do
-    field :id, non_null(:uuid4)
-    field :artist_name, non_null(:string)
-    field :description, non_null(:string)
-    field :price, non_null(:float)
-    field :hash, non_null(:string)
+    field :id, non_null(:uuid4), description: "unique identifier"
+    field :artist_name, non_null(:string), description: "artist's name"
+    field :description, non_null(:string), description: "art's description"
+    field :price, non_null(:float), description: "art's price"
+    field :hash, non_null(:string), description: "art's hash"
+  end
+
+  input_object :create_art_input do
+    field :artist_name, non_null(:string), description: "artist's name"
+    field :description, non_null(:string), description: "art's description"
+    field :price, non_null(:float), description: "art's price"
+    field :hash, non_null(:string), description: "art's hash"
   end
 end
